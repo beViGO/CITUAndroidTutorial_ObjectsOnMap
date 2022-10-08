e9ce8681b177fbc6bfca3d21abc3db35c9c2eb94
vigo <vigo.marakui@gmail.com>
2022-10-08 15:16:09 +0500

    Initial version. Button with toast message.

---
+++ app/src/main/java/ru/poopitcorp/tutorialapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.poopitcorp.tutorialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "Something Here", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
